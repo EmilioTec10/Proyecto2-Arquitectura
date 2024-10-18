151d518eeb8c8931d7f18afe541d5f866d31f448
EmilioTec10 <110855627+EmilioTec10@users.noreply.github.com>
2024-10-17 22:07:21 -0600

    Se cambio la forma en la que se lee la imagen original en el controlador VGA

--- VGA/contador_direccion.sv
+++ VGA/contador_direccion.sv
@@ -1,21 +1,26 @@
-module contador_direccion #(parameter n = 18) (input logic clk,      
-                                               input logic reset,  
-                                               input [9:0] x, 
-                                               input [9:0] y,     
-                                               output logic we,
-                                               output reg [n-1:0] out);  
-                                               
+module contador_direccion #(parameter n = 18) (
+    input logic clk,
+    input logic reset,
+    input [9:0] x,
+    input [9:0] y,
+    output logic we,
+    output reg [n-1:0] out
+);
+    
     // Variables
     reg [n-1:0] contador = 0;
     logic flag_contador = 0;
+    integer base_address;
+    integer block_x, block_y;
+    integer local_x, local_y;
 
-    always @ (posedge clk or posedge reset) begin
+    always @(posedge clk or posedge reset) begin
         if (reset) begin
             contador <= 0;
             flag_contador <= 0;
             we <= 0;
         end else begin
-            // Activar contador cuando x = 120 y y = 40
+            // Activar contador cuando x = 120 y y = 40 (primer píxel)
             if ((x == 120) && (y == 40)) begin
                 contador <= 0;
                 flag_contador <= 1;
@@ -24,15 +29,20 @@ module contador_direccion #(parameter n = 18) (input logic clk,
 
             // Incrementar el contador mientras estemos en la región de la imagen
             else if (flag_contador) begin
-                if ((x >= 120 && x < 520) && (y >= 40 && y < 440)) begin
-                    contador <= contador + 1;
-                end
+                // Calcular el bloque actual (ajustando a la posición de inicio en x = 120, y = 40)
+                block_x = (x - 120) / 100;
+                block_y = (y - 40) / 100;
+                base_address = (block_y * 4 + block_x) * 10000;
 
-                // Desactivar contador cuando se haya cubierto toda la imagen
-                else if (x == 520 && y == 440) begin
-                    contador <= 0;
+                // Calcular la posición dentro del bloque actual
+                local_x = (x - 120) % 100;
+                local_y = (y - 40) % 100;
+                contador <= base_address + local_y * 100 + local_x;
+
+                // Desactivar el contador cuando se haya recorrido toda la imagen
+                if ((x == 520) && (y == 440)) begin
                     flag_contador <= 0;
-                    we <= 0;  // Desactivar escritura
+                    we <= 0;
                 end
             end
         end
@@ -41,9 +51,3 @@ module contador_direccion #(parameter n = 18) (input logic clk,
     assign out = contador;  // Dirección de la ROM basada en el contador
 
 endmodule
-
-
-
-
-
-	
